ccf7b9e8e9bfeb86e2483a38a1ad3f5f107416ba
Habib <habibleo861@gmail.com>
2025-08-04 19:13:26 +0600

    index

--- backend/index.js
+++ backend/index.js
@@ -1,41 +1,43 @@
-import express, { urlencoded } from "express";
-import connectDB from "./db/connection.js";
+// index.js
+
+import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import bodyParser from "body-parser";
+
+import connectDB from "./db/connection.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
-const PORT = process.env.PORT || 5000;
-const app = express();
+
+// Load environment variables
 dotenv.config();
 
-// connect db
- await connectDB();
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+// Connect to the database
+connectDB();
+
+// Middleware
 app.use(cors());
-
-
-
-// middleware
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
-// API routes
+// API Routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-
-app.get('/', (req, res) => {
-  res.send('API server is running');
+// Health check
+app.get("/", (req, res) => {
+  res.send("API server is running");
 });
 
+// Start server
 app.listen(PORT, () => {
-  console.log(`server running at port ${PORT}`);
+  console.log(`Server running at port ${PORT}`);
 });
